389e773a5f54f155c4f27ed0dba199ecdfaae5da
VasukiElsa <vasuki.janarthanan6@gmail.com>
2024-03-21 20:46:04 +0530

    Add serializers file for json data

---
+++ backend/blog/serializers.py
@@ -0,0 +1,16 @@
+
+from rest_framework import serializers
+
+from blog.models import HistoricalEcho ,Contents
+
+class HistoricalEchoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HistoricalEcho
+        fields = ('_id', 'title', 'description','created_at')
+
+class ContentsSerializer(serializers.ModelSerializer):
+    historical_echo = HistoricalEchoSerializer(read_only=True)
+
+    class Meta:
+        model = Contents
+        fields = ('_id','label', 'value', 'historical_echo')
