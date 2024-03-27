136cd4927299e645ab7228b5aa56cfca983bc0ba
VasukiElsa <vasuki.janarthanan6@gmail.com>
2024-03-27 19:44:35 +0530

    Use useLocation hook to get state's result and title data:

--- frontend/src/Blogs.js
+++ frontend/src/Blogs.js
@@ -1,26 +1,31 @@
+import { useLocation } from 'react-router-dom';
 
-const Blogs = ({ data }) => {
-    console.log("Called blogs file");
-    
+const Blogs = () => {
+
+    const location = useLocation();
+    const { result, title } = location.state;
+
     return(
-        
-        <>
-        <h2>Contents</h2>
-        {data.map((content, index) => (
-            <div key={index}>
+        <div>
+            <h2>{title}</h2>
+            {result ? (
+                <ul>
+                    { result.map((item, index) => (
+                        <li key={index}>
+                            <p>{item.label}</p>
+                            <p>{item.value}</p>
+                        </li>
+                        
+                    ))}
+                </ul>
 
-               <ul>
-                <li>{content.label}</li>
-                <li>{content.value}</li>
-              </ul>
+            ) : (
+                <p>No data received</p>
+            )
+        }
 
-            </div>
-
-        ))}
-
-        </>
-
+        </div>
     );
-}
+};
 
 export default Blogs;
