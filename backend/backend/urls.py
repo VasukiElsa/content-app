ec2a856c2ce29befe3367eee2c006ddc7e467ca3
VasukiElsa <vasuki.janarthanan6@gmail.com>
2024-03-28 22:12:23 +0530

    Add urlpattern for serving image files

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from blog.views import HistoricalEchoView, ContentsView
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'historical_echo', HistoricalEchoView)
@@ -12,3 +14,5 @@ urlpatterns = [
     path('', include(router.urls)),
    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
