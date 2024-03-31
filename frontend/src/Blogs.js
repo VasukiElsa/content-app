acbe9a0abfc7b5ba943a8ed02baa16cef547b3c1
VasukiElsa <vasuki.janarthanan6@gmail.com>
2024-03-31 20:39:34 +0530

    Render cover image and replace escape characters with <br/> tag

--- frontend/src/Blogs.js
+++ frontend/src/Blogs.js
@@ -3,18 +3,29 @@ import { useLocation } from 'react-router-dom';
 const Blogs = () => {
 
     const location = useLocation();
-    const { result, title } = location.state;
+    const { result, title, cover } = location.state;
+    console.log(location);
+
+    const formatValue = (value) =>{
+        const formattedValue = value.replace(/\r\n|\r|\n/g, '<br />');
+        
+        return { __html: formattedValue };
+    };
 
     return(
-        <div>
-            <h2>{title}</h2>
+        <div class="blog-container">
+
+            <h2 className="blog-title">{title}</h2>
+            <img className="blog-cover" src={cover} />
+
             {result ? (
                 <ul>
-                    { result.map((item, index) => (
-                        <li key={index}>
-                            <p>{item.label}</p>
-                            <p>{item.value}</p>
+                    { result.map((item, index) => (        
+                        <li className="blog-list" key={index}>
+                            <p className="label-el">{item.label}</p>
+                            <p className="value-el" dangerouslySetInnerHTML={formatValue(item.value)}/>
                         </li>
+                      
                         
                     ))}
                 </ul>
