0cbb7f1c2bede1975f0d7ed518f31b9964719dac
VasukiElsa <vasuki.janarthanan6@gmail.com>
2024-04-13 12:51:23 +0530

    Define function to render index.html file
    Add url path for index page
    
    Modified urls file

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from blog.views import HistoricalEchoView, ContentsView
+from blog.views import HistoricalEchoView, ContentsView, index
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 router = routers.DefaultRouter()
 router.register(r'historical_echo', HistoricalEchoView)
@@ -11,7 +12,8 @@ router.register(r'contents',ContentsView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('', index, name='index'),
+    path('api/', include(router.urls)),
    
 ]
 
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -3,6 +3,11 @@ from rest_framework import viewsets
 from .serializers import HistoricalEchoSerializer, ContentsSerializer
 from .models import HistoricalEcho, Contents
 
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "index.html")
+
 class HistoricalEchoView(viewsets.ModelViewSet):
     serializer_class = HistoricalEchoSerializer
     queryset = HistoricalEcho.objects.all()
